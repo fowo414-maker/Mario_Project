f6ead3dc5086db6de3e10e3f15f1513fceb3b528
tayha1011 <160646200+KthDlsTK@users.noreply.github.com>
2026-07-06 13:19:13 +0900

    Refactor player movement with Rigidbody physics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
@@ -5,20 +6,59 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float moveSpeed = 5f;
+    public float jumpPower = 8f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+    private float moveInput;
+    private bool jumpRequested = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveInput = Input.GetAxisRaw("Horizontal");
+        moveInput = Input.GetAxisRaw("Horizontal");
 
-        transform.position +=
-            Vector3.right *
-            moveInput *
-            moveSpeed *
-            Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jumpRequested = true;
+        }
+
+        
+
+        
     }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (jumpRequested)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpPower);
+            jumpRequested = false;
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
 }
